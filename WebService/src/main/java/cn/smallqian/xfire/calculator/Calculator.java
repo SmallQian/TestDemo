dd2d2ede97dd112a7329a942aba9a142e103d0e6
qianqw <978482919@qq.com>
2016-04-21 20:54:35 +0800

    WebService  原始项目

---
+++ WebService/src/main/java/cn/smallqian/xfire/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package cn.smallqian.xfire.calculator;
+
+/**
+ * cn.smallqian.xfire.calculator.Calculator
+ * @author 钱琦玮 <br/>
+ * Create at 2016年4月17日 下午6:59:27
+ */
+public interface Calculator {
+
+	public int add(int a,int b);
+	
+	public int subtract(int a,int b);
+	
+	public int multiplication(int a,int b);
+	
+	public int division(int a,int b);
+	
+}
---
+++ WebService/src/main/java/cn/smallqian/xfire/calculator/impl/CalculatorInt.java
@@ -0,0 +1,36 @@
+package cn.smallqian.xfire.calculator.impl;
+
+import cn.smallqian.xfire.calculator.Calculator;
+
+/**
+ * cn.smallqian.xfire.calculator.impl.CalculatorInt
+ * @author 钱琦玮 <br/>
+ * Create at 2016年4月17日 下午7:04:59
+ */
+public class CalculatorInt implements Calculator {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public int subtract(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+	@Override
+	public int multiplication(int a, int b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+
+	@Override
+	public int division(int a, int b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+
+}
