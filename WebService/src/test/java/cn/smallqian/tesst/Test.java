b8005b27d79407ed443520d317be81044899743c
qianqw <978482919@qq.com>
2016-04-24 21:46:16 +0800

    smallqian 删除本地库

--- WebService/src/test/java/cn/smallqian/tesst/Test.java
+++ WebService/src/test/java/cn/smallqian/tesst/Test.java
@@ -4,5 +4,9 @@ public class Test {
 	
 	public static void main(String argss[]){
 		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
 	}
 }
