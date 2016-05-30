466e4efece590caadb4e28c6c9b2d56e9f29c5d7
qianqw <978482919@qq.com>
2016-05-30 19:41:20 +0800

    hehe

---
+++ WebService/src/main/java/cn/smallqian/xfire/hello/impl/HelloImpl.java
@@ -0,0 +1,12 @@
+package cn.smallqian.xfire.hello.impl;
+
+import cn.smallqian.xfire.hello.Hello;
+
+/**
+ * cn.smallqian.xfire.hello.impl.HelloImpl
+ * @author 钱琦玮 <br/>
+ * Create at 2016年5月30日 下午7:36:44
+ */
+public class HelloImpl implements Hello {
+
+}
