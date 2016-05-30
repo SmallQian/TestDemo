96505b7a141a1fa5060f4bfebf64beb1ef488c80
qianqw <978482919@qq.com>
2016-05-30 19:35:28 +0800

    hello implement hell

---
+++ WebService/src/main/java/cn/smallqian/cxf/hello/HelloWord.java
@@ -0,0 +1,10 @@
+package cn.smallqian.cxf.hello;
+
+/**
+ * cn.smallqian.cxf.hello.HelloWord
+ * @author 钱琦玮 <br/>
+ * Create at 2016年5月30日 下午7:02:44
+ */
+public interface HelloWord {
+
+}
---
+++ WebService/src/main/java/cn/smallqian/cxf/hello/impl/HelloWordImpl.java
@@ -0,0 +1,12 @@
+package cn.smallqian.cxf.hello.impl;
+
+import cn.smallqian.cxf.hello.HelloWord;
+
+/**
+ * cn.smallqian.cxf.hello.impl.HelloWordImpl
+ * @author 钱琦玮 <br/>
+ * Create at 2016年5月30日 下午7:03:15
+ */
+public class HelloWordImpl implements HelloWord{
+
+}
